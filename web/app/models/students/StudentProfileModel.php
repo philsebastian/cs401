d3ddf9e608ff6ab7ae15d187d59210caf7611bb6
philsebastian <philsebastian@u.boisestate.edu>
2018-04-16 17:53:50 -0600

    More views

--- web/app/models/students/StudentProfileModel.php
+++ web/app/models/students/StudentProfileModel.php
@@ -10,11 +10,7 @@ class StudentProfileModel extends StudentsModel
     public function GetData($content)
     {
         $data = parent::GetData($content);
-        $tabcontent = array("tabs" => ["profile" => "account", "Payment Account" => "account/paymentinfo", "payment history" => "account/payments", "my teacher" => "account/myteacher"]);
-        $accountInfo = parent::GetMyStudentAccountInfo();
-
-        $data = array_merge($data, $content);
-        $data = array_merge($data, $accountInfo);
+        $tabcontent = array("tabs" => ["profile" => "account", "payment account" => "account/paymentinfo", "payment history" => "account/payments", "my teacher" => "account/myteacher"]);
         $data = array_merge($data, $tabcontent);
 
         return $data;
--- web/app/models/students/StudentsModel.php
+++ web/app/models/students/StudentsModel.php
@@ -12,19 +12,68 @@ class StudentsModel extends PageModels
         $glyphs  = ['logout' => 'log-out'];
         parent::__construct($name, $primary,  $control, $glyphs, STUDENTROOT);
     }
- 
+
     public function GetData($content)
     {
-        return parent::GetData($content);
+        $data = parent::GetData($content);
+
+        if(isset($content['view']))
+        {
+            $displayInfo = self::GetViewData($content);
+            $data = array_merge($data, $displayInfo);
+        }
+
+        return $data;
     }
 
-    public function GetMyStudentAccountInfo()
+    private function GetViewData($content)
+    {
+        $displayInfo = [];
+        try
+        {
+            $part = $content['view'];
+            $part = ucwords($part);
+            $part = str_replace(' ', '', $part);
+            $searchMethod = "Get{$part}Info";
+            $displayInfo = self::$searchMethod();
+        }
+        catch (Exception $ex)
+        {
+            Logger::LogError("StudentsModel.GetViewData", "Error: {$ex->getMessage()}");
+            $_SESSION['errorMessage'] = "Error retrieving data. Please try again.";
+            exit(header("Location: " . STUDENTROOT));
+        }
+        return $displayInfo;
+    }
+
+    protected function GetProfileInfo()
     {
         //$session = new Session();
         return $this->session->GetMyUserAccountInfo($_SESSION['usernameId']);
     }
-    public function GetMyTeachers()
+    protected function GetMyTeacherInfo()
     {
-        return $this->session->GetMyTeachers($_SESSION['usernameId']);
+        //return $this->session->GetMyTeachers($_SESSION['usernameId']);
+        return ['myteacher' => []];
+    }
+    protected function GetPaymentAccountInfo() // PHIL TODO -- to implement
+    {
+        return ['paymentaccount' => []];
+    }
+    protected function GetPaymentHistoryInfo() // PHIL TODO -- to implement
+    {
+        return ['paymenthistory' => []];
+    }
+    protected function GetUpcomingInfo()
+    {
+        return ['appointments' => []];
+    }
+    protected function GetHistoryInfo()
+    {
+        return ['history' => []];
+    }
+    protected function GetScheduleInfo()
+    {
+        return ['schedule' => []];
     }
 }
